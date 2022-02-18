e5e72716a8afcefea55f6f1a3fa6dab3c7ff71eb
Catarina Rinaldi <catarinarinaldi@hotmail.com>
2022-02-18 12:12:01 -0300

    @catarinaldi [Adicionando routes]

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CadastrarComponent } from './cadastrar/cadastrar.component';
+import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
-  // {path:'', redirectTo: 'login', pathMatch: 'full'},
-  // {path: 'login', component: loginComponent},
-  // {path: 'cadastrar', component: cadastrarComponent},
+  {path:'', redirectTo: 'login', pathMatch: 'full'},
+  {path: 'login', component: LoginComponent},
+  {path: 'cadastrar', component: CadastrarComponent},
 ];
 
 @NgModule({
