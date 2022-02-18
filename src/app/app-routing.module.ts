54786eae935ce5f4c0b7772525e9fee8e03c4546
Catarina Rinaldi <catarinarinaldi@hotmail.com>
2022-02-18 11:54:38 -0300

    @catarinaldi [Adicionando login e cadastro]

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  // {path:'', redirectTo: 'login', pathMatch: 'full'},
+  // {path: 'login', component: loginComponent},
+  // {path: 'cadastrar', component: cadastrarComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
